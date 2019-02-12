3ad0dc07001fbfda11b46fe814298dd7161a110d
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-12 14:04:53 -0500

    Added messages
    Using messages to prompt user to give input.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,6 +9,19 @@ public class Main {
     //Inches to Millimeters is x*25.4
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! Please type your name...");
+        String name = scan.nextLine();
+
+        System.out.println("Well hello "+name+", it's nice to meet you. ");
+
+        System.out.println("I'm here to calculate your BMI. Please type your height in feet.");
+        System.out.println(" Don't worry about inches, that is the next question");
+        Integer inches = scan.nextInt();
+
+        System.out.println("Okay. Next type the remaining inches.");
+
+        System.out.println("Next, please type your weight in pounds. Using decimals is fine.");
         
+
     }
 }
