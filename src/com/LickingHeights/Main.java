cf229425cc643e66aab050c770be2c431046fec8
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-12 13:58:33 -0500

    Created scanner class
    Created scanner class for user input.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,14 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//Feet to Meters is x/3.2808
+    //Inches to Millimeters is x*25.4
+
+        Scanner scan = new Scanner(System.in);
+        
     }
 }
